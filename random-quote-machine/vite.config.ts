9c4364d395c7f851623b8c3cd6adef0b73ea2b7a
Michael <f0978899827@gmail.com>
2025-06-23 19:46:37 +0800

    專案移出docs目錄

--- random-quote-machine/vite.config.ts
+++ random-quote-machine/vite.config.ts
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/Front-End-Development-Libraries_FCC-Projects/docs/random-quote-machine/',
   plugins: [react()],
 })
